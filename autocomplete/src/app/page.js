3287b931f5c2948c93c0a418562c85be1d7ba61c
bochrap <131978926+bochrap@users.noreply.github.com>
2024-02-06 15:47:49 +0000

    fetch autocomplete

--- autocomplete/src/app/page.js
+++ autocomplete/src/app/page.js
@@ -1,4 +1,5 @@
 "use client";
+import Asynchronous from "./components/AsynchronousReq";
 import ComboBox from "./components/ComboBox";
 
 export default function Home() {
@@ -6,6 +7,9 @@ export default function Home() {
     <div>
       <p>Selece an emplyee profile you want to access </p>
       <ComboBox />
+      <br />
+      <p>Select music album from the list:</p>
+      <Asynchronous />
     </div>
   );
 }
