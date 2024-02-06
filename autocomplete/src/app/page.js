8058027d07d0cf53b076a3b19c167f24bec70d12
bochrap <131978926+bochrap@users.noreply.github.com>
2024-02-06 14:50:54 +0000

    add combo box autocomplete

--- autocomplete/src/app/page.js
+++ autocomplete/src/app/page.js
@@ -1,15 +1,11 @@
 "use client";
 import ComboBox from "./components/ComboBox";
-// import CountrySelect from "./CountrySelect";
-import ButtonUsage from "./components/Button";
 
 export default function Home() {
   return (
     <div>
-      <ButtonUsage />
+      <p>Selece an emplyee profile you want to access </p>
       <ComboBox />
-      {/* <CountrySelect />
-      // <ComboBox /> */}
     </div>
   );
 }
